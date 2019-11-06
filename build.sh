7eecc286a2f35646b0e2d4f649715c5355c0cc4e
CF @invist <cf@l3l.lol>
2019-11-06 12:42:30 +0100

    added poormans check; improvment is pgp sign

--- build.sh
+++ build.sh
@@ -5,3 +5,5 @@ cd ..
 goxc;
 docker build . -t $(jq '"invist/lel:" + .version' static/package.json)
 docker-compose -f docker-compose.yml.build up --build
+
+find . -path "./pkg/*" -name "LeL_*" -type f -exec sha256sum {} \; > pkg/LeL/$(jq '.version' static/package.json)/SHA256SUMS
