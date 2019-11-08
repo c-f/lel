76f961392644a81ebf3f0662ea412946a6603fa9
CF @invist <cf@l3l.lol>
2019-11-08 16:47:20 +0100

    ability to search tags

--- static/src/components/LelNavPanel/loop.jsx
+++ static/src/components/LelNavPanel/loop.jsx
@@ -10,6 +10,7 @@ export const loop = (data, searchValue, previous) => {
       if (item == null) {
         return null;
       }
+      let tagFound = false;
 
       const index = item.title.indexOf(searchValue);
       const beforeStr = item.title.substr(0, index);
@@ -25,8 +26,16 @@ export const loop = (data, searchValue, previous) => {
           <span>{item.title}</span>
         );
 
+      if (searchValue.startsWith("tags:")) {
+        if (item.other !== undefined && item.other.meta !== undefined) {
+          tagFound = item.other.meta.names.some(i => {
+            return `tags:${i}`.indexOf(searchValue) >= 0;
+          });
+        }
+      }
+
       // display all children if folder was found
-      const prevFound = previous || index > -1;
+      const prevFound = previous || index > -1 || tagFound;
 
       if (item.children) {
         const children = loop(item.children, searchValue, prevFound).filter(
@@ -43,7 +52,7 @@ export const loop = (data, searchValue, previous) => {
           );
         }
       }
-      if (index > -1 || searchValue == "" || prevFound) {
+      if (index > -1 || searchValue == "" || prevFound || tagFound) {
         if (item.other !== undefined && item.other.meta !== undefined) {
           const names = item.other.meta.names.map(i => {
             return <Tag key={`${item.key}-${i}`}>{i} </Tag>;
