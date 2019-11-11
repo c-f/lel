7cd7973678b329383d1fded18939ef5d41f3a9db
CF @invist <cf@l3l.lol>
2019-11-11 12:14:21 +0100

    fixed build script

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ cd static;
 npm run build;
 cd ..
 goxc;
-docker build . -t $(jq '"invist/lel:" + .version' static/package.json)
+docker build . -t $(jq -r '"invist/lel:" + .version' static/package.json)
 docker-compose -f docker-compose.yml.build up --build
 
 find . -path "./pkg/*" -name "LeL_*" -type f -exec sha256sum {} \; > pkg/LeL/$(jq '.version' static/package.json)/SHA256SUMS
